a868c23ec1320f1152959b146bf8d87db1e82f9a
Saumya <manidecp@gmail.com>
2024-04-07 09:11:55 +0000

    init code done

--- src/main/java/com/nfr/record/controller/PostController.java
+++ src/main/java/com/nfr/record/controller/PostController.java
@@ -1,7 +1,9 @@
 package com.nfr.record.controller;
 
+import com.google.gson.Gson;
 import com.nfr.record.entity.Post;
 import com.nfr.record.interfaces.PostService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Scanner;
 
 
 @RestController
@@ -19,9 +23,23 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    static String extractPostRequestBody(HttpServletRequest request) {
+        if ("POST".equalsIgnoreCase(request.getMethod())) {
+            Scanner s = null;
+            try {
+                s = new Scanner(request.getInputStream(), "UTF-8").useDelimiter("\\A");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return s.hasNext() ? s.next() : "";
+        }
+        return "";
+    }
+
     @PostMapping("/create-new-post")
-    private ResponseEntity<?> createNewPost(@RequestBody Post post) {
+    private ResponseEntity<?> createNewPost(HttpServletRequest request) {
         try {
+            Post post = new Gson().fromJson(extractPostRequestBody(request), Post.class);
             HashMap<String, Object> responseObject = postService.createPost(post);
             return ResponseEntity.ok(responseObject);
         } catch (Exception ex) {
@@ -29,3 +47,4 @@ public class PostController {
         }
     }
 }
+
